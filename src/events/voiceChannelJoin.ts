da33b297329c84f49cee4e0bad161b5a92e3b1ba
sev-ofc <47048628+sev-ofc@users.noreply.github.com>
2021-07-01 09:40:01 -0700

    Update voiceChannelJoin.ts

--- src/events/voiceChannelJoin.ts
+++ src/events/voiceChannelJoin.ts
@@ -4,9 +4,8 @@ const logs = require('discord-logs');
 logs(bot);
 
 bot.on(`voiceChannelJoin`, async (member: Discord.GuildMember, channel: Discord.VoiceChannel) => {
-	console.log('yo it fired')
 	if(member.id == bot.user.id || member.user.bot || channel.members.size == 1 || ! channel.members ||! channel.joinable ||! channel.speakable) return;
-
+	if(member.guild.id !== '635582459366342659') return;
 
 	let constellation = await bot.guilds.cache.get('635582459366342659');
 	let achannel = await constellation.channels.cache.get('653035130738311188');
