b95fa9bbe340e2631b87eac9f37e00237894acf1
sev-ofc <47048628+sev-ofc@users.noreply.github.com>
2021-07-01 18:50:51 -0700

    Update queue.ts

--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -11,12 +11,12 @@ export let commandProps = {
 export async function cmd(message: Message, args: string[]){
 	let thisQueue = await MusicPlayer.getQueue(message);
 	let msg = `:book: **Bot Queue**\nCurrently Playing: ${MusicPlayer.nowPlaying(message).title}`
-	
 	let i = 1
 	thisQueue.tracks.forEach(async track => {
 		if(i - 1 == thisQueue.tracks.length) return message.reply(msg)
 		msg.concat(`\n${i}: ${track.title} [${track.duration}]`)
 		i++
 	})
+	return message.reply(msg)
 
 }
