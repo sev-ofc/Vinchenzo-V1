7a7cd93b2da1df1fac60c75380907bda8636b1f3
sev-ofc <47048628+sev-ofc@users.noreply.github.com>
2021-07-01 18:56:07 -0700

    First 10 items in Q

--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -10,10 +10,11 @@ export let commandProps = {
 
 export async function cmd(message: Message, args: string[]){
 	let thisQueue = await MusicPlayer.getQueue(message);
-	let msg = `:book: **Bot Queue**\nCurrently Playing: ${MusicPlayer.nowPlaying(message).title}`
+	let msg = `:book: **Bot Queue [First 10 Items]**\nCurrently Playing: ${MusicPlayer.nowPlaying(message).title}`
 	let i = 1
 	thisQueue.tracks.forEach(async track => {
 		msg = msg.concat(`\n${i}: ${track.title} [${track.duration}]`)
+		if(i = 10) return;
 		i++
 	})
 	return message.reply(msg)
