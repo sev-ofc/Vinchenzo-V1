e981bc7d2c2cb3d7245d3a054df7b1cee98df634
sev <47048628+sev-ofc@users.noreply.github.com>
2021-07-15 22:03:04 -0700

    Finalizing

--- src/modules/SusTimer.ts
+++ src/modules/SusTimer.ts
@@ -18,24 +18,23 @@ export class SusTimer {
     }
 
     check(roleDurationMillS: number) {
-        // console.log(`[TIMERS] Checking sus timer...`);
         let currentTimestamp = Date.now();
         bot.guilds.cache.forEach(async guild => {
             let config = await GuildMastero.getConfig(guild.id);
             for(let prop in config.sussyusers){
-                console.log(`Checking prop ${prop} versus ${currentTimestamp} || Time Passed in MS: ${currentTimestamp - parseInt(prop)} || Duration MS: ${roleDurationMillS}`)
+                // console.log(`Checking prop ${prop} versus ${currentTimestamp} || Time Passed in MS: ${currentTimestamp - parseInt(prop)} || Duration MS: ${roleDurationMillS}`)
                 /* Current Timestamp: 5000
                 *  Timestamp Assigned to role: 4000
                 *  5000 - 4000 = 1000 MS passed since
                 */
                 if(currentTimestamp - parseInt(prop) >= roleDurationMillS){
-                    console.log("Gotta remove that role ig")
+                    // console.log("Gotta remove that role ig")
                     let memberToRemoveRole = await guild.members.fetch(config.sussyusers[prop])
                     try {
                         let susRole = guild.roles.cache.find(role => role.name.toLowerCase() == "sus");
                         if(!susRole) continue;
                         memberToRemoveRole.roles.remove(susRole)
-                        if(memberToRemoveRole.nickname.length > 2) memberToRemoveRole.setNickname(memberToRemoveRole.user.username);
+                        if(memberToRemoveRole.nickname.toLowerCase().includes("sus")) memberToRemoveRole.setNickname(memberToRemoveRole.user.username);
                         delete config.sussyusers[prop];
                         GuildMastero.updateConfig(guild.id, config);
                     }
