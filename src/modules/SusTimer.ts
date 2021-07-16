23ca74e132cc43b53127833dcca89a4c3231e860
sev <47048628+sev-ofc@users.noreply.github.com>
2021-07-15 21:55:38 -0700

    Update SusTimer.ts

--- src/modules/SusTimer.ts
+++ src/modules/SusTimer.ts
@@ -12,22 +12,27 @@ export class SusTimer {
         this.roleDurationMS = roleDuartion;
 
         console.log(`[TIMERS] Sus timer set to check every ${Math.floor(this.repeatMS / 60000).toString()} minutes! (Role Duration: ${Math.floor(this.roleDurationMS / 60000).toString()} Minutes)`);
-        this.check();
-        bot.setInterval(this.check, msToRepeat)
+        this.check(roleDuartion);
+        
+        function loopCycle() {
+            this.check(roleDuartion);
+        }
+
+        bot.setInterval(loopCycle, msToRepeat);
     }
 
-    check() {
+    check(roleDurationMillS: number) {
         // console.log(`[TIMERS] Checking sus timer...`);
         let currentTimestamp = Date.now();
         bot.guilds.cache.forEach(async guild => {
             let config = await GuildMastero.getConfig(guild.id);
             for(let prop in config.sussyusers){
-                console.log(`Checking prop ${prop} versus ${currentTimestamp} || Time Passed in MS: ${currentTimestamp - parseInt(prop)} || Duration MS: ${this.roleDurationMS}`)
+                console.log(`Checking prop ${prop} versus ${currentTimestamp} || Time Passed in MS: ${currentTimestamp - parseInt(prop)} || Duration MS: ${roleDurationMillS}`)
                 /* Current Timestamp: 5000
                 *  Timestamp Assigned to role: 4000
                 *  5000 - 4000 = 1000 MS passed since
                 */
-                if(currentTimestamp - parseInt(prop) >= this.roleDurationMS){
+                if(currentTimestamp - parseInt(prop) >= roleDurationMillS){
                     console.log("Gotta remove that role ig")
                     let memberToRemoveRole = await guild.members.fetch(config.sussyusers[prop])
                     try {
