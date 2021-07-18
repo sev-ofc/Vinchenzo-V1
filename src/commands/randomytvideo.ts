f8aa1d9290d488c0fafb2fe09d1d82718030814f
sev <47048628+sev-ofc@users.noreply.github.com>
2021-07-17 23:28:02 -0700

    Random YT Vid test

---
+++ src/commands/randomytvideo.ts
@@ -0,0 +1,18 @@
+import { Player } from "discord-player";
+import { Message } from "discord.js";
+import {bot, MusicPlayer} from '../index';
+import youtube from 'youtube-random-video';
+
+export let commandProps = {
+	'name': 'randomytvideo',
+	'usage': '[prefix]randomytvideo',
+	'aliases': ['randomyoutubevid', 'randomyoutubevideo','randomvid', 'randomyt', 'randomytvid']
+}
+
+export async function cmd(message: Message, args: string[]){
+    let apikey = process.env.YOUTUBE_API_KEY;
+    youtube.getRandomVid(apikey, function(err , data){
+        if(err) message.reply(":x: An error occured! Please alert the bot owner about this command not working!");
+        else console.log(data);
+    })
+}
