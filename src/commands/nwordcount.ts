a9284c508f73994321b063e464bb3baa7a79f3c7
sev <47048628+sev-ofc@users.noreply.github.com>
2021-07-17 23:10:37 -0700

    Update nwordcount.ts

--- src/commands/nwordcount.ts
+++ src/commands/nwordcount.ts
@@ -20,7 +20,7 @@ export async function cmd(message: Message, args: string[]){
     if(message.guild.members.cache.find(member => member.user.username.toLowerCase() == args[0].toLowerCase() || member.user.id == args[0].toLowerCase()) || message.mentions.users.size > 0){
         let member = await bot.users.cache.find(member => member.username.toLowerCase() == args[0].toLowerCase() || member.id == args[0].toLowerCase()) || message.mentions.users.first();
         if(!message.guild.members.cache.has(member.id)) message.reply(":x: You must specify a user in this server!");
-        return message.reply(`:white_check_mark: User **${member.tag}** has said the N-word ${between(0, 1060327)} times!`)
+        return message.reply(`:white_check_mark: User **${member.username}** has said the N-word **${between(0, 1060327)}** times!`)
     }
     else return message.reply(":x: You must specify who to check the count of!");   
 }
