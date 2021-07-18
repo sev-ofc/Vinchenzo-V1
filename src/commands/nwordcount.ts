b1e3e293777752c1b1331986eb308090be465129
sev <47048628+sev-ofc@users.noreply.github.com>
2021-07-17 23:11:45 -0700

    Update nwordcount.ts

--- src/commands/nwordcount.ts
+++ src/commands/nwordcount.ts
@@ -14,13 +14,18 @@ function between(min, max) {
     )
   }
 
+// https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
   // Meme command - not serious. It actually is a random number
 export async function cmd(message: Message, args: string[]){
     if(!args[0]) return message.reply(":x: You must specify who to check the count of!");
     if(message.guild.members.cache.find(member => member.user.username.toLowerCase() == args[0].toLowerCase() || member.user.id == args[0].toLowerCase()) || message.mentions.users.size > 0){
         let member = await bot.users.cache.find(member => member.username.toLowerCase() == args[0].toLowerCase() || member.id == args[0].toLowerCase()) || message.mentions.users.first();
         if(!message.guild.members.cache.has(member.id)) message.reply(":x: You must specify a user in this server!");
-        return message.reply(`:white_check_mark: User **${member.username}** has said the N-word **${between(0, 1060327)}** times!`)
+        return message.reply(`:white_check_mark: User **${member.username}** has said the N-word **${numberWithCommas(between(0, 100060327))}** times!`)
     }
     else return message.reply(":x: You must specify who to check the count of!");   
 }
